32a0498cf9ce756bede978c5ef47d58b22149374
Pavithra Rajendran <pavithra.rajendran@gosh.nhs.uk>
2024-08-22 15:17:27 +0100

    updated v1.2.0

--- pretrained_word_embeddings/config.py
+++ pretrained_word_embeddings/config.py
@@ -3,7 +3,7 @@
 import os
 from pathlib import Path
 
-LOCAL_FILES = True
+LOCAL_FILES = False
 
 DATAPATH = os.path.join(Path().resolve().parent, "nlp_models")
 
@@ -13,8 +13,8 @@ WORD_ORDERING = False
 # pretrained embedding vectors word2vec format or not
 W2VFORMAT = True
 
-MODEL_CLASSIFICATION = r"C:\Users\rajenp\Desktop\projects\genomics_workstream\open-publication-repo\genomics_nlp_pipeline\data\nlp_models\biobert-nli"
+MODEL_CLASSIFICATION = os.path.join(DATAPATH, "biobert-nli")
 MODEL_THRESHOLD_CLASSIFICATION = 0.50
 
-MODEL_CONCLUSION =  r"C:\Users\rajenp\Desktop\projects\genomics_workstream\open-publication-repo\genomics_nlp_pipeline\data\nlp_models\multi-qa-MiniLM-L6-cos-v1"
+MODEL_CONCLUSION = os.path.join(DATAPATH, "multi-qa-MiniLM-L6-cos-v1")
 MODEL_THRESHOLD_CONCLUSION = 0.50
